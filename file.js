4b9ccbbd0d1a68e2cdf424135730763685b79442
Mohammed Haider Fadhil <109572248+Mohammedalhamami@users.noreply.github.com>
2024-06-07 17:25:44 +0300

    Update file.js
    i used ES6 using template string

--- file.js
+++ file.js
@@ -1,12 +1,12 @@
-var color1 = document.querySelector(".color1");
-var color2 = document.querySelector(".color2");
-var css = document.querySelector("h3");
-var body = document.querySelector("body");
+const color1 = document.querySelector(".color1");
+const color2 = document.querySelector(".color2");
+const css = document.querySelector("h3");
+const body = document.querySelector("body");
 
 function changeColor() {
   body.style.background =
-    "linear-gradient(to right, " + color1.value + ", " + color2.value + ")";
-  css.textContent = body.style.background + ";";
+    `linear-gradient(to right, ${color1.value}, ${color2.value})`;
+  css.textContent = `${body.style.background} ;`;
 }
 
 color1.addEventListener("input", changeColor);
